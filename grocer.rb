d399d854a5e019fba5eec5ad0faf54e9f82cdad2
Joel Mendoza <joelmendza173@gmail.com>
2019-07-31 02:47:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ def checkout(cart, coupons)
   # code here
   total = 0
   nCart = consolidate_cart(cart)
-  coup_cart = apply_clearance(consolidate_cart(cart))
+  coup_cart = apply_clearance(nCart)
   nCart.each{|hash|
     total+=hash[1][:price]
   }
